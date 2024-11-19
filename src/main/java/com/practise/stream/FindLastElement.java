72b9f71c223850e08358feb103a7429b71b1eff8
Monikaa06 <monikaaprabu@gmail.com>
2024-11-19 07:16:56 +0530

    JL stream part 4

--- src/main/java/com/practise/stream/FindLastElement.java
+++ src/main/java/com/practise/stream/FindLastElement.java
@@ -9,8 +9,10 @@ public class FindLastElement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7); 
-		Integer last = numbers.stream().sorted(Collections.reverseOrder()).findFirst().orElse(0);
+		Integer last = numbers.stream().sorted(Collections.reverseOrder()).skip(1).findFirst().orElse(0);
 		System.out.println(last);
+		Integer last2 = numbers.stream().skip(numbers.size()-1).findFirst().orElse(0);
+		System.out.println(last2);
 	}
 
 }
